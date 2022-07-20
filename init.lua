8f3970c1405aa7533ed4fc8e14852e773e6432b6
João Gonçalves <joaogoncalves8@hotmail.com>
2022-07-20 11:54:29 +0100

    (nvim) fix

--- init.lua
+++ init.lua
@@ -124,32 +124,34 @@ require('packer').startup(function(use)
     use {
         'kyazdani42/nvim-tree.lua',
         requires = { 'kyazdani42/nvim-web-devicons' },
-        require('nvim-tree').setup({
-            renderer = {
-                icons = {
-                    glyphs = {
-                        default = "",
-                        symlink = "",
-                        git = {
-                            unstaged = "",
-                            staged = "S",
-                            unmerged = "",
-                            renamed = "➜",
-                            deleted = "",
-                            untracked = "U",
-                            ignored = "◌",
-                        },
-                        folder = {
-                            default = "",
-                            open = "",
-                            empty = "",
-                            empty_open = "",
-                            symlink = "",
+        config = function()
+            require('nvim-tree').setup({
+                renderer = {
+                    icons = {
+                        glyphs = {
+                            default = "",
+                            symlink = "",
+                            git = {
+                                unstaged = "",
+                                staged = "S",
+                                unmerged = "",
+                                renamed = "➜",
+                                deleted = "",
+                                untracked = "U",
+                                ignored = "◌",
+                            },
+                            folder = {
+                                default = "",
+                                open = "",
+                                empty = "",
+                                empty_open = "",
+                                symlink = "",
+                            }
                         }
                     }
                 }
-            }
-        })
+            })
+        end
     }
 
     -- startup screen
